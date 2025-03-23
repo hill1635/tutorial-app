696f483d60687fc28eec02ef6e7a402e457cfd1e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-03-23 12:47:59 -0600

    Added Header, Footer, and Navigation components.

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <img src="/logo192.png" alt="Logo" />
+      <h1>Welcome to the Internet</h1>
+    </header>
+  );
+}
+export default Header;
