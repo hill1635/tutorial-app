b6f76ca8b17d9fa03555f44927b268cec492f22e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-03-23 12:54:17 -0600

    Imported Navigation component.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Navigation from './Navigation';
 
 const Header = () => {
+  const HEADER_LINKS = [
+    { label: 'Home', path: '/' },
+    { label: 'Login', path: '/login' },
+    { label: 'Sign Up', path: '/signup' },
+  ];
+
   return (
     <header>
       <img src="/logo192.png" alt="Logo" />
       <h1>Welcome to the Internet</h1>
+      <Navigation links={HEADER_LINKS} />
     </header>
   );
 }
