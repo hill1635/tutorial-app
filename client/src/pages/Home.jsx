64a62afa08ff4f784d169e58f38553e50ad299ac
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-03-23 12:42:36 -0600

    Created Home page.

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+      <section>
+        <h1>Hello, friend!</h1>
+        <p>Welcome to the website.</p>
+      </section>
+      <section>
+        <h2>You're probably here because you're wondering:</h2>
+        <ul>
+          <li>What is internet?</li>
+          <li>How does it?</li>
+          <li>But also, why?</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Maybe we can answer those questions</h3>
+        <p>But also, maybe not.  We could just make it more confusing.</p>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
