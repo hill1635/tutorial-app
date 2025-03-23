fcf360be8585e9716ac4d3340fe0fc33a11a4770
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-03-23 12:54:03 -0600

    Created Navigation component.

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navigation = (props) => {
+  return (
+    <nav>
+      <ul>
+        {props?.links.map((link, index) => (
+          <li key={index}>
+            <a href={
+              link.path
+            }>{link.label}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
