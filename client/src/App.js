5ddf2c8263086f35fb27669a415dd921317e00fd
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-03-23 12:49:26 -0600

    Imported Header component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './pages/Home';
+import Header from './components/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
