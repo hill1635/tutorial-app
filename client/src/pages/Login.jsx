8965bd8fa43f0f05ff0cb972744bdba2d797e88e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-04-01 20:37:12 -0600

    Created login page.

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import EmailForm from '../components/EmailForm';
+
+const Login = () => {
+  const login = (input) => {
+    console.log("Login input:", input);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <EmailForm onSubmit={login}/>
+    </div>
+  );
+}
+
+export default Login;
