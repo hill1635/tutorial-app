2656059d0e98de522c29d99eefd53d9a09b29945
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-04-01 22:22:18 -0600

    Added middleware to parse requests.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const session = require('express-session');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
 
 const PORT = 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
