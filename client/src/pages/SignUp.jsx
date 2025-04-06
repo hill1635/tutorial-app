38f1bdcd66ef0d921e176fba94ce02cebeafcd6e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-04-06 15:19:55 -0600

    Added notifications state.

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import EmailForm from '../components/EmailForm';
 import UserAPI from '../utils/UserAPI';
+import { useState } from 'react';
 
 const SignUp = () => {
+  const [notifications, setNotifications] = useState([]);
   const signUp = (input) => {
-    UserAPI.create(input);
+    UserAPI.create(input)
+    .then(res => {
+      if (res.data.messages) {
+        setNotifications(res.data.messages);
+      }
+    });
   };
 
   return (
