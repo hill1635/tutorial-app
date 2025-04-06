05ed7dbbf93c2c9539c001128100d358a21a24fb
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-04-06 11:38:46 -0600

    Imported user model and added functionality to create user.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,24 @@
 // require encryptController from controllers/encrypt.js
 const encrypt = require('./encryptController');
+const User = require('../models/user');
 
 module.exports = {
   createUser: async (req, res) => {
     const { email, password } = req.body;
     const salt = await encrypt.generateSalt(10);
     const hash = await encrypt.hash(password, salt);
+    
+    User.create({
+      email: email,
+      password: hash,
+    })
+    .then((dbUser) => {
+      console.log('User created:', dbUser);
+      res.json(dbUser);
+    })
+    .catch((err) => {
+      console.error('Error creating user:', err);
+      res.status(500).json(err);
+    });
   },
 };
