b10362af1b4da92f4df4a8cba5c2a7ab2608409c
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-04-26 11:08:15 -0600

    Converted backend to module type.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+import { Router } from 'express';
+import userRoutes from './userRoutes.js';
+
+const router = Router();
 
 router.use('/user', userRoutes);
 
-module.exports = router;
+export default router;
