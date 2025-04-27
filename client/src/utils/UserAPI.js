3d843e0c9a3f58939037376f96c65e6a7f6f1a73
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-04-27 09:25:47 -0600

    Created login api endpoint.

--- client/src/utils/UserAPI.js
+++ client/src/utils/UserAPI.js
@@ -4,6 +4,9 @@ const UserAPI = {
   create: function (userData) {
     return axios.post('/api/user/', userData);
   },
+  login: function (userData) {
+    return axios.post('/api/user/login', userData);
+  },
 };
 
 export default UserAPI;
