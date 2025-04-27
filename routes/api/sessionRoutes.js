bb1d15a10806c53a557b46b7fdac208319e439f7
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-04-27 10:30:43 -0600

    Added route to get session.

--- routes/api/sessionRoutes.js
+++ routes/api/sessionRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { login } from '../../controllers/sessionController.js';
+import { login, getSession } from '../../controllers/sessionController.js';
 
 const router = Router();
 
+router.route('/').get(getSession);
 router.route('/login').post(login);
 
 export default router;
