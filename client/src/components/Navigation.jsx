f2f6625f41a5bb7be2f5c1ca9f880c83c5b749cf
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-04-27 11:52:20 -0600

    Filtered links based on session status and link show property.

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useContext, useMemo } from 'react';
+import { SessionContext } from '../context/SessionContext';
 
 const Navigation = (props) => {
+  const { session } = useContext(SessionContext);
+
+  const filteredLinks = useMemo(() => {
+    return props?.links.filter((link) => {
+      return (
+        link.show === 'always' ||
+        (session.id && link.show === 'signedIn') ||
+        (!session.id && link.show === 'signedOut')
+      );
+    });
+  }, [session, props.links]);
+
   return (
     <nav>
       <ul>
-        {props?.links.map((link, index) => (
+        {filteredLinks.map((link, index) => (
           <li key={index}>
             <a href={link.path}>{link.label}</a>
           </li>
