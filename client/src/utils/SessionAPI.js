10eb7c9e227a4b946eddf87a65840845776145f2
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-04-27 10:30:58 -0600

    Added api endpoint for get session.

--- client/src/utils/SessionAPI.js
+++ client/src/utils/SessionAPI.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const SessionAPI = {
+  getSession: function () {
+    return axios.get('/api/session/');
+  },
   login: function (userData) {
     return axios.post('/api/session/login', userData);
   },
